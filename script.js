d356c3e57d46da83887226d56d91295c4cfb3b8e
rohanpiya <piyar@dickinson.edu>
2026-06-16 13:18:52 +0545

    Add logic to the game

--- script.js
+++ script.js
@@ -15,3 +15,35 @@ function getHumanChoice() {
     }
     return userPrompt;
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    if (
+        (humanChoice == "scissors" && computerChoice == "paper") ||
+        (humanChoice == "paper" && computerChoice == "rock") ||
+        (humanChoice == "rock" && computerChoice == "scissors")
+    ) {
+        console.log(`You win this round! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+    } else if (humanChoice == computerChoice) {
+        console.log(`This round is a draw!`)
+    } else {
+        console.log(`You lose this round! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    }
+}
+
+function playGame() {
+    while (humanScore < 5 && computerScore < 5) {
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+    } if (computerScore == 5) {
+        console.log("You lose the game :(");
+    } else {
+        console.log("Congrats! YOu win this game :)");
+    }
+}
