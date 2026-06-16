ba84183332ee40e50066538a1d27d105859be953
rohanpiya <piyar@dickinson.edu>
2026-06-16 12:58:09 +0545

    Add getHumanChoice function to the file

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 function getComputerChoice() {
-    randNum = Math.random();
+    let randNum = Math.random();
     if (randNum <= 0.33) {
         return "rock";
     } else if (randNum <= 0.66) {
         return "paper";
     }
     return "scissors";
+}
+
+function getHumanChoice() {
+    let userPrompt = prompt("Please enter your choice: ");
+    while (userPrompt != "rock" && userPrompt != "paper" && userPrompt != "scissors") {
+        userPrompt = prompt("Invalid. Please make a valid choice: ");
+    }
+    return userPrompt;
 }
