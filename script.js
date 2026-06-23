21081929fd4cff956bfdc599fa671bef293a1aad
rohanpiya <piyar@dickinson.edu>
2026-06-23 15:24:56 +0545

    Add game logic

--- script.js
+++ script.js
@@ -26,26 +26,42 @@ function playRound(humanChoice, computerChoice) {
         (humanChoice == "paper" && computerChoice == "rock") ||
         (humanChoice == "rock" && computerChoice == "scissors")
     ) {
-        console.log(`You win this round! ${humanChoice} beats ${computerChoice}`);
-        humanScore++;
+        return "human";
     } else if (humanChoice == computerChoice) {
-        console.log(`This round is a draw!`)
+        return "draw";
     } else {
-        console.log(`You lose this round! ${computerChoice} beats ${humanChoice}`);
-        computerScore++;
+        return "computer";
     }
 }
 
-function playGame() {
-    humanScore = 0;
-    computerScore = 0;
-    while (humanScore < 5 && computerScore < 5) {
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    } if (computerScore == 5) {
-        console.log("You lose the game :(");
-    } else {
-        console.log("Congrats! YOu win this game :)");
-    }
-}
+// function playGame() {
+//     humanScore = 0;
+//     computerScore = 0;
+//     while (humanScore < 5 && computerScore < 5) {
+//         let humanSelection = getHumanChoice();
+//         let computerSelection = getComputerChoice();
+//         playRound(humanSelection, computerSelection);
+//     } if (computerScore == 5) {
+//         console.log("You lose the game :(");
+//     } else {
+//         console.log("Congrats! YOu win this game :)");
+//     }
+// }
+
+const choiceButtons = document.querySelectorAll('.choice-btn');
+const resultsDiv = document.querySelector('.results');
+
+choiceButtons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        humanSelection = btn.textContent;
+        computerSelection = getComputerChoice();
+        result = playRound(humanSelection, computerSelection);
+        if (result == 'draw') {
+            resultsDiv.textContent = "It's a draw!!";
+        } else if (result == "human") {
+            resultsDiv.textContent = `Human wins! ${humanSelection} beats ${computerSelection}`;
+        } else {
+            resultsDiv.textContent = `Computer wins! ${humanSelection} loses to ${computerSelection}`;
+        }
+    })
+})
